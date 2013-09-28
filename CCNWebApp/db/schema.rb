ebf7a8360c6f0817321a98db11e3120d178ba650
Thao Nguyen <thaonguyen@Thaos-MacBook-Air.local>
2013-09-28 18:59:46 -0500

    min's commit

--- CCNWebApp/db/schema.rb
+++ CCNWebApp/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130928201352) do
+ActiveRecord::Schema.define(version: 20130928235915) do
 
   create_table "calories", force: true do |t|
     t.integer  "intake"
     t.integer  "outtake"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "donations", force: true do |t|
