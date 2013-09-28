114b2ed612bd520da4f98da4a7694f38d251976d
ntran2 <ntran2@uno.edu>
2013-09-28 08:56:00 -0500

    rails build and devise

---
+++ CCNWebApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CCNWebApp::Application.config.secret_key_base = 'd93bf3c245dc067ad48226c19e8428d091d13898513f70f7b385aadd64766a4956763ef933d2344ca7c1087af53391142e2cbd2b90466df4cb9ffc087a2b8752'
---
+++ CCNWebApp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CCNWebApp::Application.config.session_store :cookie_store, key: '_CCNWebApp_session'
